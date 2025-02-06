3522989b2d7f1df6e54328ede34f97d4ea083b0a
Prashu2181 <prasannamadireddy466@gmail.com>
2025-02-06 13:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  let count = {};
+  for(let i = 0; i < str.length; i++){
+    let char = str[i];
+    if(count[char] === undefined){
+      count[char] = 1;
+    }
+    else{
+      count[char] = count[char] + 1;
+    }
+  }
+
+
+for(let i = 0; i < str.length; i++){
+  if(count[str[i]] === 1){
+    return str[i];
+  }
+}
+return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
